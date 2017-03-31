9a90839c57c856d9d469c779c61f2a323a2f1aea
a3zzat <viva.amego@gmail.com>
2017-03-31 16:01:23 +0200

    add configuration bits

--- MplabX_template/Template.X/main.c
+++ MplabX_template/Template.X/main.c
@@ -1,10 +1,40 @@
 /*
  * File:   main.c
- * Author: CoBoL
+ * Author: Ahmed
  *
  * Created on March 31, 2017, 3:41 PM
  */
 
+// PIC16LF1614 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = ECH       //  (ECH, External Clock, High Power Mode (4-20 MHz): device clock supplied to CLKIN pins)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit cannot be cleared once it is set by software)
+#pragma config ZCD = OFF        // Zero Cross Detect Disable Bit (ZCD disable.  ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PLLEN = ON       // PLL Enable Bit (4x PLL is always enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS1F// WDT Period Select (Software Control (WDTPS))
+#pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
+#pragma config WDTCWS = WDTCWSSW// WDT Window Select (Software WDT window size control (WDTWS bits))
+#pragma config WDTCCS = SWC     // WDT Input Clock Selector (Software control, controlled by WDTCS bits)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
 
